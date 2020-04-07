74499a645df9eaf1fa696e7a4cf4fb7dd4cf8171
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-04-07 18:01:47 -0500

    added delete endpoint

--- router.js
+++ router.js
@@ -24,6 +24,7 @@ Comment schema
 
 */
 
+//create a post
 router.post('/', (req, res) => {
   if (!req.body.title || !req.body.contents){
     res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
@@ -39,7 +40,7 @@ router.post('/', (req, res) => {
   }
 })
 
-//add comment for a post
+//add comment for a specific post
 router.post('/:id/comments', (req, res) => {
   db.findById(req.params.id)
   .then(post => {
@@ -61,10 +62,63 @@ router.post('/:id/comments', (req, res) => {
       })
     }
   })
-  .catch(err => {
-    res.status(500).json({ error: "There was an error while saving the comment to the database."})
+})
+
+//get request for posts
+router.get('/', (req, res) => {
+  db.find()
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(err => res.status(500).json({ error: "The posts information could not be retrieved."}))
+})
+
+router.get('/:id', (req, res) => {
+  db.findById(req.params.id)
+  .then(post => {
+    if (!post){
+      res.status(404).json({ message: "The post with the specified ID was not found"})
+    } else {
+      res.status(200).json(post)
+    }
+  })
+  .catch(err => res.status(500).json({ error: "The posts information could not be retrieved."}))
+})
+
+router.get('/:id/comments', (req, res) => {
+  db.findById(req.params.id)
+  .then(post => {
+    if(!post){
+      res.status(404).json({ message: "The post with the specified ID does not exist."})
+    } else {
+      db.findCommentById(req.params.id)
+      .then(comments => {
+        res.status(200).json(comments)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "The comments information could not be retrieved."
+        })
+      })
+    }
   })
 })
+
+router.delete('/:id', (req, res) => {
+  let deletedPost; 
+  db.findById(req.params.id)
+  .then(post => {
+    deletedPost = post
+    console.log(deletedPost)
+  })
+  db.remove(req.params.id)
+  .then(item => {
+    console.log(item)
+    res.json(deletedPost)
+  }) 
+})
+
+
 
 
 module.exports = router
